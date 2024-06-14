915cc010e7abd3e18272b43b523990be2233cb69
Sahil Agashe <sahilagashe2002@gmail.com>
2024-06-14 22:50:20 +0530

    Implemented MainTabController and ScannerController

---
+++ Movies App/Controller/MainTabController.swift
@@ -0,0 +1,25 @@
+//
+//  MainTabController.swift
+//  Movies App
+//
+//  Created by SAHIL AMRUT AGASHE on 14/06/24.
+//
+
+import UIKit
+
+class MainTabController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let moviesController = MoviesViewController(viewModel: MovieListViewModel(httpClient: HTTPClient()))
+        moviesController.tabBarItem = UITabBarItem(title: "Home", image: UIImage(systemName: "house"), selectedImage: UIImage(systemName: "house.fill"))
+        
+        let scannerController = ScannerController()
+        scannerController.tabBarItem = UITabBarItem(title: "Scanner", image: UIImage(systemName: "scanner"), selectedImage: UIImage(systemName: "scanner.fill"))
+
+        viewControllers = [moviesController, scannerController]
+        
+        tabBar.backgroundColor = .systemBackground
+    }
+}
---
+++ Movies App/Controller/ScannerController.swift
@@ -0,0 +1,17 @@
+//
+//  ScannerController.swift
+//  Movies App
+//
+//  Created by SAHIL AMRUT AGASHE on 14/06/24.
+//
+
+import UIKit
+
+class ScannerController: UITabBarController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .white
+    }
+}
