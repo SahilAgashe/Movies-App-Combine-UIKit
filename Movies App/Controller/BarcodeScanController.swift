416f3b063d682b973cbefcfe91345968bfcd37cf
Sahil Agashe <sahilagashe2002@gmail.com>
2024-06-15 13:21:57 +0530

    Scanning Barcode is done! Delegate method is running!

--- Movies App/Controller/BarcodeScanController.swift
+++ Movies App/Controller/BarcodeScanController.swift
@@ -6,7 +6,9 @@
 //
 
 import UIKit
+import AVFoundation
 
+private let kDebugBarcodeScanController = "DEBUG BarcodeScanController: "
 class BarcodeScanController: UIViewController {
     
     
@@ -21,16 +23,99 @@ class BarcodeScanController: UIViewController {
         return btn
     }()
     
+    private var captureDevice: AVCaptureDevice? = AVCaptureDevice.default(for: AVMediaType.video)
+    private var captureSession: AVCaptureSession = AVCaptureSession()
+    private lazy var captureVideoPreviewLayer: AVCaptureVideoPreviewLayer = {
+        let layer = AVCaptureVideoPreviewLayer(session: captureSession)
+        layer.videoGravity = .resize
+        layer.connection?.videoOrientation = .portrait
+        return layer
+    }()
+    
+    public var metadata = [
+        AVMetadataObject.ObjectType.upce,
+        AVMetadataObject.ObjectType.code39,
+        AVMetadataObject.ObjectType.code39Mod43,
+        AVMetadataObject.ObjectType.ean13,
+        AVMetadataObject.ObjectType.ean8,
+        AVMetadataObject.ObjectType.code128,
+        AVMetadataObject.ObjectType.code93,
+        AVMetadataObject.ObjectType.pdf417,
+        AVMetadataObject.ObjectType.qr,
+        AVMetadataObject.ObjectType.aztec
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        view.backgroundColor = .red
-        view.addSubview(cancelButton)
-        cancelButton.anchor(top: view.safeAreaLayoutGuide.topAnchor, right: view.rightAnchor, paddingTop: 20, paddingRight: 20 ,width: 90, height: 35)
+        configureUI()
+        requestForCameraAccess()
     }
     
     @objc func cancelButtonAction() {
         dismiss(animated: true)
     }
     
+    private func configureUI() {
+        captureVideoPreviewLayer.frame = view.layer.bounds
+        view.layer.addSublayer(captureVideoPreviewLayer)
+        
+        view.addSubview(cancelButton)
+        cancelButton.anchor(top: view.safeAreaLayoutGuide.topAnchor, right: view.rightAnchor, paddingTop: 20, paddingRight: 20 ,width: 90, height: 35)
+    }
+    
+    private func requestForCameraAccess() {
+        
+        // Get permission for Camera
+        AVCaptureDevice.requestAccess(for: .video) { [weak self] granted in
+            
+            if granted {
+                print("REQUEST GRANTED!")
+                
+                DispatchQueue.main.async { [weak self] in
+                    self?.setupCaptureSession()
+                }
+                
+            } else {
+                print("NOT ALLOWED!")
+            }
+        }
+        
+    }
+    
+    private func setupCaptureSession() {
+        guard let captureDevice 
+        else {
+            print(kDebugBarcodeScanController, "Capture Device Not Found!")
+            return
+        }
+        
+        // Adding capture device input in capture session
+        do {
+            let input = try AVCaptureDeviceInput(device: captureDevice)
+            captureSession.addInput(input)
+        } catch {
+            print(kDebugBarcodeScanController, "capture device input error: \(error.localizedDescription)")
+        }
+        
+        // Adding capture device output in capture session
+        let output = AVCaptureMetadataOutput()
+        output.setMetadataObjectsDelegate(self, queue: DispatchQueue.main)
+        // NOTE: - add output to session first then set metadata types otherwise will get crash!
+        captureSession.addOutput(output)
+        output.metadataObjectTypes = metadata
+        
+       
+
+        // Running capture session
+        DispatchQueue.global(qos: .background).async { [weak self] in
+            self?.captureSession.startRunning()
+        }
+    }
+    
+}
+
+extension BarcodeScanController: AVCaptureMetadataOutputObjectsDelegate {
+    func metadataOutput(_ output: AVCaptureMetadataOutput, didOutput metadataObjects: [AVMetadataObject], from connection: AVCaptureConnection) {
+        print(kDebugBarcodeScanController, #function)
+    }
 }
